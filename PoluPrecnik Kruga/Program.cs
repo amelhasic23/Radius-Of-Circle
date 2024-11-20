3c5d84ee2882dcc479347a981bf958c6cb1fd653
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-20 10:22:44 +0100

    Commit

---
+++ PoluPrecnik Kruga/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double PI = 3.14;
+        double r = 5;
+        double area = PI * r * r;
+
+        Console.WriteLine($"Površina kruga sa poluprečnikom {r} je: {area}");
+    }
+}
+
